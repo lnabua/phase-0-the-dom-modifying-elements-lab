80e1bf53cbbfa7f19a6447a0e2b380dc1f9bc69c
lnabua <nabua.lorraine@gmail.com>
2021-02-26 04:13:52 -0800

    commit tree 137af8701109edf5ec54b4abc4a8575493535fb6

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+document.querySelector('main').remove();
+
+const newHeader = document.createElement('h1');
+document.body.appendChild(newHeader);
+
+newHeader.id = 'victory';
+newHeader.innerHTML = 'Lorraine is the champion';
